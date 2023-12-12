ea6a4edad8f87738dea267b7b26cd03ae31de213
Manjima94 <manji5570@gmail.com>
2023-12-12 11:59:59 +0530

    dart

---
+++ gratestnbr.dart
@@ -0,0 +1,18 @@
+void main() {
+  int a = 54, b = 71, c = 33;
+  if (a > b) {
+    if (a > c) {
+      print('$a is greatest ');
+    }
+  }
+  if (b > a) {
+    if (b> c) {
+      print('$b is greatest ');
+}
+  }
+  if (c > b) {
+    if (c > a) {
+      print('$c is greatest ');
+}
+  }
+}
---
+++ smalernbr.dart
@@ -0,0 +1,18 @@
+void main(){
+  int a=5,b=7,c=9;
+  if(a<b){
+    if(a<c){
+      print('$a is smaller');
+    }
+  }
+   if(b<a){
+    if(b<c){
+      print('$b is smaller');
+    }
+  }
+   if(c<a){
+    if(c<b){
+      print('$c is smaller');
+    }
+  }
+}
